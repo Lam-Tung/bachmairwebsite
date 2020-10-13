99db2717891fcb51f777d87411d4ffc123df027b
brotato <lamtung.truong@gmail.com>
2020-10-13 16:27:09 +0200

    scenarios added with pdf download

--- src/components/LearnMobile.vue
+++ src/components/LearnMobile.vue
@@ -1,70 +1,65 @@
 <template>
     <div class="space-y-4">
-        <div class="text-xl ml-4 mr-4 border-4 border-indigo-900 rounded md:ml-12 md:mr-12 lg:ml-64 lg:mr-64">
+        <div @click="isDefinitionOpen = !isDefinitionOpen" class="text-xl ml-4 mr-4 border-4 border-indigo-900 rounded cursor-pointer md:ml-12 md:mr-12 lg:ml-64 lg:mr-64">
             <h1 class="definitioncard">Was ist mobiles Lernen?</h1>
-            <p class="p-4 lg:p-8">
+            <div v-if="!isDefinitionOpen">
+                <p class="p-4 lg:p-8">
+                    Mobiles Lernen zielt auf Lehr- und Lernformen, die medial-digitale Veränderungen in der Lebenswelt der Schülerinnen und Schüler in die Schule integrieren. Zur Lebenswelt gehören Handys ebenso wie Tablets. Welches didaktische Potential bieten Mobile Endgeräten als neue Kulturressource? Wie lässt sich dieses Potential mit Formen des situierten kooperierenden Lernens verbinden? (...mehr)
+                </p>
+            </div>
+            <div v-if="isDefinitionOpen">
+                <p class="p-4 lg:p-8">
                 In der ersten Annäherung an eine Definition Mobilen Lernens handelt es sich um Lernen, bei dem die Anwendungsfunktionen mobiler digitaler Endgeräte eine Rolle spielen. Wenn Leseanfänger Wörter ihrer Lebenswelt wie Müllverbrennungsanlage mit dem Handy fotografieren und diese Fotos in der Grundschule ausdrucken, dann darüber reden, kommt die Fotofunktion des Handys beim Leselernen zum Tragen.
-            </p>
-            <p class="p-4 lg:p-8">
-                In einer zweiten Annäherung geht es nicht um die technischen Anwendungsfunktionen von Handy, Smartphone oder Tablet. Es steht vielmehr die Einbindung der mobilen Endgeräte in unsere Gesellschaft im Vordergrund. So sind Handy und Smartphone in den Alltag integriert. Smartphone und Tablet sind individueller Teil der Medienkonvergenz mit dem Internet. Tablets als technologische Abkömmlinge des PC stellen die Brücke zu bekannten Formen des digitalen Arbeitens her. Alle mobilen Endgeräte sind auf Kommunikation und Kooperation ausgelegt. Zumindest Handy und Smartphone sind ubiquitär, also im Prinzip jederzeit und überall individuell verfügbar. Diese Funktionen mobiler Endgeräte bieten eine Anbindung von Schule an informelles Lernen des Alltags, Möglichkeiten kooperativen Lernens, situiertes Lernen über Kontexte hinweg (context aware learning).
-            </p>
-            <p class="p-4 lg:p-8">
-                Um zu einer fundierten Definition mobilen Lernens zu kommen, ist es hilfreich die Passung (englisch: Affordance) der digitalen Mobil-Technologie an gesellschaftliche Strukturen wie Individualisierung und Ökonomisierung, dem Akteur-Status der Lernenden und den Kulturpraktiken des Lernens und Lehrens zu analysieren.
-            </p>
+                </p>
+                <p class="p-4 lg:p-8">
+                    In einer zweiten Annäherung geht es nicht um die technischen Anwendungsfunktionen von Handy, Smartphone oder Tablet. Es steht vielmehr die Einbindung der mobilen Endgeräte in unsere Gesellschaft im Vordergrund. So sind Handy und Smartphone in den Alltag integriert. Smartphone und Tablet sind individueller Teil der Medienkonvergenz mit dem Internet. Tablets als technologische Abkömmlinge des PC stellen die Brücke zu bekannten Formen des digitalen Arbeitens her. Alle mobilen Endgeräte sind auf Kommunikation und Kooperation ausgelegt. Zumindest Handy und Smartphone sind ubiquitär, also im Prinzip jederzeit und überall individuell verfügbar. Diese Funktionen mobiler Endgeräte bieten eine Anbindung von Schule an informelles Lernen des Alltags, Möglichkeiten kooperativen Lernens, situiertes Lernen über Kontexte hinweg (context aware learning).
+                </p>
+                <p class="p-4 lg:p-8">
+                    Um zu einer fundierten Definition mobilen Lernens zu kommen, ist es hilfreich die Passung (englisch: Affordance) der digitalen Mobil-Technologie an gesellschaftliche Strukturen wie Individualisierung und Ökonomisierung, dem Akteur-Status der Lernenden und den Kulturpraktiken des Lernens und Lehrens zu analysieren.
+                </p>
+            </div>
         </div>
         <div class="text-xl ml-4 mr-4 border-4 border-indigo-900 rounded md:ml-12 md:mr-12 lg:ml-64 lg:mr-64">
-            <h1 class="text-center text-2xl font-semibold p-4">Stichworte zum mobilen Lernen: Alltag, Schule, Ökonomie und Kultur</h1>
+            <h1 class="definitioncard">Stichworte zum mobilen Lernen: Alltag, Schule, Ökonomie und Kultur</h1>
             <p class="p-4 lg:p-8">
                 Mobiles Lernen bedeutet in einer ersten Definition, die neuen mobilen Digitaltechnologien, das sind im Moment vor allem Smartphones und Tablets, in Lernkontexten wie Schule oder für informelles Lernen im Alltag zu nutzen. Dafür gibt es auf den ersten Blick einen guten Grund: Weltweit trägt nahezu jeder ein Handy oder das internetfähig Smartphone bei sich. Dennoch verlangt Schule weltweit, Handys und Smartphones in der Schule auszuschalten. Mit Engagement unterstützen Lehrerinnen und Lehrer dieses Handy-Verbot.
             </p>
-            <button @click="downloadWithAxios(src, title)" class="ref text-center p-2">
+            <a href="pdfs/BachmairMobilesLernen8August14.pdf" download class="ref text-center p-2">
                 Volltext hier als PDF herunterladen
-            </button>
+            </a>
+        </div>
+        <div class="">
+            <div class="plusminus">
+                <h1 @click="isScenariosOpen = !isScenariosOpen" class="text-3xl underline font-semibold cursor-pointer">Szenarien</h1>
+                <button @click="isScenariosOpen = !isScenariosOpen" class=" block focus:outline-none p-4">
+                <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path v-if="!isScenariosOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <path v-if="isScenariosOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                </button>
+            </div>
+            <div v-if="isScenariosOpen">
+                <LearnMobileScenarios/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 
-import axios from 'axios';
+import LearnMobileScenarios from '@/components/LearnMobileScenarios.vue';
 
 export default {
     name: "LearnMobile",
+    components: {
+        LearnMobileScenarios,
+    },
     data() {
         return {
-            title : 'WasIstMobilesLernen.png',
-            src : require('../assets/pictures/bachmair.png'),
-            
-            filesrcs: [
-                {
-                    title: 'WasIstMobilesLernen.pdf',
-                    src: 'http://ben-bachmair.de/Mobiles_Lernen_-_Was_ist_mobiles_Lernen_files/Bachmair%20Mobiles%20Lernen%208August14.pdf',
-                },
-                
-            ],
+            isDefinitionOpen: false,
+            isScenariosOpen: false,
+            //pdfLink: require("@/assets/pdfs/BachmairMobilesLernen8August14.pdf"),
         } // end return
     }, // end data
-    methods: {
-        forceFileDownload(response, title) {
-            console.log(title)
-            const url = window.URL.createObjectURL(new Blob([response.data]))
-            const link = document.createElement('a')
-            link.href = url
-            link.setAttribute('download', title)
-            document.body.appendChild(link)
-            link.click()
-        },
-        downloadWithAxios(url, title) {
-            axios({
-                method: 'get',
-                url,
-                responseType: 'arraybuffer',
-            })
-                .then((response) => {
-                this.forceFileDownload(response, title)
-                })
-                .catch(() => console.log('error occured'))
-        },
-  } // end methods
 } // end export
 </script>
---
+++ src/components/LearnMobileScenarios.vue
@@ -0,0 +1,110 @@
+<template>
+    <div>
+        <ul class="mt-4 lg:ml-64 lg:mr-64">
+            <li v-for="scenario in scenarios" :key="scenario.scenarioName" class="border-solid border-indigo-900 border-4 rounded ml-4 mr-4 mt-4 shadow-md">
+                <div class="pubname">
+                    {{scenario.scenarioName}}
+                </div>
+                <div class="text-center mt-2">
+                    {{scenario.author}}
+                </div>
+                <div v-if="scenario.download !== null" class="text-center mt-2">
+                    <a :href="scenario.download" class="ref">Download</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LearnMobileScenarios",
+    data() {
+        return {
+            scenarios: [
+                {
+                    scenarioName: 'WhatsApp for Gender and Language Awareness (2015)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Bachmair-Scenarios Augsburg 2015 awareness+context.pdf',
+                },
+                {
+                    scenarioName: 'Awareness Path (2014)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Bachmair awareness path ba 19Oct14.pdf',
+                },
+                {
+                    scenarioName: 'Learning in and by generated, provisional Contexts (2014)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Bachmair 5 Nov 14 learning in and by generated context.pdf',
+                },
+                {
+                    scenarioName: 'Agency and Life Orientation of Social Justice (2014)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Bachmair 5Nov 14 examples on social justice.pdf',
+                },
+                {
+                    scenarioName: 'Zielorientiertes Schüler-Portfolio Übergangsklasse (2014)',
+                    author: 'Ben Bachmair',
+                    download: null,
+                },
+                {
+                    scenarioName: 'Partner Interview (2013)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Bachmair-partner interview- 8Dez13.pdf',
+                },
+                {
+                    scenarioName: 'Multimodal Bricolage (2013)',
+                    author: 'Ben Bachmair, Norbert Pachler',
+                    download: 'pdfs/Bachmair multimodal bricolage-text+image workshop.pdf',
+                },
+                {
+                    scenarioName: 'Auf dem Weg zu einer Didaktik mobilen Lernens (2013)',
+                    author: 'Ben Bachmair',
+                    download: null,
+                },
+                {
+                    scenarioName: 'Hausaufgaben (2013)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Hausaufgaben 2013 Ben Bachmair.pdf',
+                },
+                {
+                    scenarioName: 'Botanische Garten / Botanical Garden (2012)',
+                    author: 'Ben Bachmair',
+                    download: null, //TODO eng and ger
+                },
+                {
+                    scenarioName: 'Training Scenario - MyMobile (2012)',
+                    author: 'Ben Bachmair, Norbert Pachler',
+                    download: 'pdfs/Scenario 1 from MyMobile 2012.pdf',
+                },
+                {
+                    scenarioName: 'Bicycle Workshop (2011)',
+                    author: 'Ben Bachmair, Norbert Pachler',
+                    download: 'pdfs/Bachmair Bicycle workshop 2011.pdf',
+                },
+                {
+                    scenarioName: 'T-Shirt Images (2011)',
+                    author: 'Ben Bachmair',
+                    download: 'pdfs/Bachmair t-shirt images 2011.pdf',
+                },
+                {
+                    scenarioName: 'Buchstaben und Laute suchen (2011)',
+                    author: 'Ben Bachmair, Katja Friedrich, Maren Risch',
+                    download: null,
+                },
+                {
+                    scenarioName: 'Mit dem Handy Geräusche einfangen (2011)',
+                    author: 'Ben Bachmair, Katja Friedrich, Maren Risch',
+                    download: null,
+                },
+                {
+                    scenarioName: 'Homework - Mathematic Angles (2011)',
+                    author: 'Ben Bachmair, John Cook, Norbert Pachler',
+                    download: 'pdfs/',
+                },
+            ],
+        } // end return
+    }, // end data
+    
+} // end export
+</script>
